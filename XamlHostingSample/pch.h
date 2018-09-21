89a75f877e7f8ec684905b51bb4bb4044faeba79
Joshua Partlow <joshuapa@microsoft.com>
2018-09-21 15:58:02 -0700

    Initial release

---
+++ XamlHostingSample/pch.h
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+#ifndef PCH_H
+#define PCH_H
+#include <iostream>
+#include <winrt/Windows.system.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Input.Inking.h>
+#include <winrt/Windows.UI.Text.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.UI.Xaml.Hosting.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+#include <winrt/Windows.Media.Core.h>
+#include <winrt/Windows.Media.Playback.h>
+#endif
